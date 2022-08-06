1cbf6e9ca4173229ad2d39d9a991b8ef1b6c9cbb
Birdcar <434063+birdcar@users.noreply.github.com>
2022-08-06 16:10:26 -0500

    Update action js

--- hello_world/dist/index.js
+++ hello_world/dist/index.js
@@ -8838,43 +8838,29 @@ function wrappy (fn, cb) {
 
 "use strict";
 
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 const core_1 = __importDefault(__nccwpck_require__(2186));
 const github_1 = __importDefault(__nccwpck_require__(5438));
-function run() {
-    return __awaiter(this, void 0, void 0, function* () {
-        try {
-            const nameToGreet = core_1.default.getInput("who-to-greet");
-            console.log(`Hello ${nameToGreet}`);
-            const time = (new Date()).toTimeString();
-            core_1.default.setOutput("time", time);
-            // get the JSON webhook payload
-            const payload = JSON.stringify(github_1.default.context.payload, undefined, 2);
-            console.log(`The event payload: ${payload}`);
-        }
-        catch (error) {
-            if (typeof error === 'string') {
-                core_1.default.setFailed(error.toUpperCase());
-            }
-            else if (error instanceof Error) {
-                core_1.default.setFailed(error.message);
-            }
-        }
-    });
+try {
+    const nameToGreet = core_1.default.getInput("who-to-greet");
+    console.log(`Hello ${nameToGreet}`);
+    const time = new Date().toTimeString();
+    core_1.default.setOutput("time", time);
+    // get the JSON webhook payload
+    const payload = JSON.stringify(github_1.default.context.payload, undefined, 2);
+    console.log(`The event payload: ${payload}`);
 }
-run();
+catch (error) {
+    if (typeof error === "string") {
+        core_1.default.setFailed(error.toUpperCase());
+    }
+    else if (error instanceof Error) {
+        core_1.default.setFailed(error.message);
+    }
+}
 
 
 /***/ }),
